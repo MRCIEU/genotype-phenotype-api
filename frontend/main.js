5620cba852493e7419f05930a386a1c7719c477b
Andrew Elmore <andrew.elmore@bristol.ac.uk>
2024-10-08 00:47:55 +0100

    Passing name to phenotype page

--- frontend/main.js
+++ frontend/main.js
@@ -1,6 +1,10 @@
 import 'bulma/css/bulma.css'
+// import * as be from 'bulma-extensions';
 import logo from './images/logo.png'
+import colocs from './images/colocs.png'
 import smallLogo from './images/small_logo.svg'
+import coloc from './sample_data/coloc.json'
+import studies from './sample_data/studies.json'
 
 import * as d3 from 'd3';
 import Alpine from 'alpinejs';
@@ -8,36 +12,40 @@ import Alpine from 'alpinejs';
 /* TODO: look into using alpine with reusable web components here: https://stackoverflow.com/questions/65710987/reusable-alpine-js-components */
 
 window.Alpine = Alpine;
-Alpine.data('app', () => ({
+Alpine.data('homepage', () => ({
   d3,
   logo,
   smallLogo,
   count: 0,
 }))
 
+Alpine.data('phenotype', (param) => ({
+  dataReceived: true,
+  colocs: colocs,
+  studyData: studies,
+  colocData: coloc,
+
+  get getStudyToDisplay() {
+    let studyId = (new URLSearchParams(location.search).get('id'))
+    const study = this.studyData.find((item) => {
+        return item.id == studyId
+    })
+    return study.name
+  },
+
+  get getDataForColocResults() {
+    this.dataReceived = true 
+    return this.colocData
+  }
+}))
+
 Alpine.data('searchInput', () => ({
   isOpen: false,
   search: '',
-  dummyData: [
-    {
-      id: 1,
-      type: 'phenotype',
-      name: 'Mean corpuscular hemoglobin',
-    },
-    {
-      id: 2,
-      type: 'phenotype',
-      name: 'Mean corpuscular hemoglobin concentration',
-    },
-    {
-      id: 3,
-      type: 'gene',
-      name: 'D2 gene',
-    }
-  ],
+  dummyData: studies,
 
   goToItem(item) {
-    window.location.href = 'phenotype.html?' + item.id;
+    window.location.href = 'phenotype.html?id=' + item.id;
     this.search = ''
   },
 
