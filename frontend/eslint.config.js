9e639d630b691cfe4000b621cc2df72f56d021bb
Andrew Elmore <andrew.elmore@bristol.ac.uk>
2025-07-31 11:50:38 +0100

    Adding in prettier packages and config

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,31 +1,34 @@
 import { defineConfig } from "eslint/config";
 import js from "@eslint/js";
 import globals from "globals";
+import prettier from "eslint-plugin-prettier";
 
 export default defineConfig([
-	{
-        languageOptions: {
-            globals: {
-                ...globals.browser,
-                Alpine: 'readonly',
-                __dirname: 'readonly',
-            },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        Alpine: "readonly",
+        __dirname: "readonly",
+      },
+    },
+    files: ["**/*.js"],
+    plugins: {
+      js,
+      prettier,
+    },
+    extends: ["js/recommended"],
+    rules: {
+      "no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          destructuredArrayIgnorePattern: "^_",
         },
-		files: ["**/*.js"],
-		plugins: {
-			js,
-		},
-		extends: ["js/recommended"],
-		rules: {
-			"no-unused-vars": [
-                "error",
-                {
-                    "argsIgnorePattern": "^_",
-                    "varsIgnorePattern": "^_",
-                    "destructuredArrayIgnorePattern": "^_"
-                }
-            ],
-			"no-undef": "warn",
-		},
-	},
+      ],
+      "no-undef": "warn",
+      "prettier/prettier": "error",
+    },
+  },
 ]);
