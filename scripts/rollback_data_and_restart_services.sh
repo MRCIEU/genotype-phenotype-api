e8b28cd4fa47c1ff72bb3f925bff4a895679b5c5
Andrew Elmore <andrew.elmore@bristol.ac.uk>
2025-08-07 11:36:03 +0100

    updating deploy scripts

--- scripts/rollback_data_and_restart_services.sh
+++ scripts/rollback_data_and_restart_services.sh
@@ -8,6 +8,9 @@ echo "Swapping back to old databases"
 mv /oradiskvdb1/db/studies.db /oradiskvdb1/db/studies_new.db
 mv /oradiskvdb1/db/studies_backup.db /oradiskvdb1/db/studies.db
 
+mv /oradiskvdb1/db/coloc_pairs.db /oradiskvdb1/db/coloc_pairs_new.db
+mv /oradiskvdb1/db/coloc_pairs_backup.db /oradiskvdb1/db/coloc_pairs.db
+
 mv /oradiskvdb1/db/associations.db /oradiskvdb1/db/associations_new.db
 mv /oradiskvdb1/db/associations_backup.db /oradiskvdb1/db/associations.db
 
@@ -18,6 +21,11 @@ echo "Swapping back to old ld blocks"
 mv /oradiskvdb1/data/ld_blocks /oradiskvdb1/data/ld_blocks_new
 mv /oradiskvdb1/data/ld_blocks_backup /oradiskvdb1/data/ld_blocks
 mkdir -p /oradiskvdb1/data/ld_blocks_new
+
+echo "Swapping back to old svgs"
+mv /oradiskvdb1/static/svgs /oradiskvdb1/static/svgs_new
+mv /oradiskvdb1/static/svgs_backup /oradiskvdb1/static/svgs
+mkdir -p /oradiskvdb1/static/svgs_new
 
 sudo docker compose up -d
 
--- scripts/update_data_and_restart_services.sh
+++ scripts/update_data_and_restart_services.sh
@@ -8,6 +8,9 @@ echo "Swapping to new databases"
 mv /oradiskvdb1/db/studies.db /oradiskvdb1/db/studies_backup.db
 mv /oradiskvdb1/db/studies_new.db /oradiskvdb1/db/studies.db
 
+mv /oradiskvdb1/db/coloc_pairs.db /oradiskvdb1/db/coloc_pairs_backup.db
+mv /oradiskvdb1/db/coloc_pairs_new.db /oradiskvdb1/db/coloc_pairs.db
+
 mv /oradiskvdb1/db/associations.db /oradiskvdb1/db/associations_backup.db
 mv /oradiskvdb1/db/associations_new.db /oradiskvdb1/db/associations.db
 
@@ -18,6 +21,11 @@ echo "Swapping to new study information for gwas upload"
 mv /oradiskvdb1/data/ld_blocks /oradiskvdb1/data/ld_blocks_backup
 mv /oradiskvdb1/data/ld_blocks_new /oradiskvdb1/data/ld_blocks
 mkdir -p /oradiskvdb1/data/ld_blocks_new
+
+echo "Swapping to new svgs"
+mv /oradiskvdb1/static/svgs /oradiskvdb1/static/svgs_backup
+mv /oradiskvdb1/static/svgs_new /oradiskvdb1/static/svgs
+mkdir -p /oradiskvdb1/static/svgs_new
 
 sudo docker compose up -d
 
