f77769ee490b817f9e82197797d5b038289ade72
Andrew Elmore <andrew.elmore@bristol.ac.uk>
2026-06-03 10:26:16 +0100

    Fixing SSL cert refresh

--- scripts/refresh_ssl_certs.sh
+++ scripts/refresh_ssl_certs.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -euo pipefail
 
+#Add this to the crontab on the server running the API
+# 30 6 * * * flock -n /tmp/gpmap-refresh-ssl.lock /home/opc/genotype-phenotype-api/refresh_ssl_certs.sh >> /var/log/gpmap-refresh-ssl.log 2>&1
+
 sudo docker service scale gpmap_certbot=1
 sleep 10
 sudo docker service update --force gpmap_frontend
